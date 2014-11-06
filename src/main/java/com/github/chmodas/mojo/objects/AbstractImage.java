861eda0a9d31aee9be2583bbf81382350cd4de6c
Borislav Borisov <bborisov@peer1.com>
2014-11-06 15:40:02 +0000

    An annoying work around maven plugin limitation
    For more information -
    http://maven.apache.org/guides/mini/guide-configuring-plugins.html. More
    specifically:
    " Alternatively, if you move a configuration section outside of the
    executions section, it will apply globally to all invocations of the
    plugin."

--- src/main/java/com/github/chmodas/mojo/objects/AbstractImage.java
+++ src/main/java/com/github/chmodas/mojo/objects/AbstractImage.java
@@ -2,8 +2,18 @@ package com.github.chmodas.mojo.objects;
 
 import org.apache.maven.plugin.MojoExecutionException;
 
+import java.util.List;
+
 abstract class AbstractImage {
     private String name;
+    private String repository;
+    private String tag;
+    private String command;
+    private String hostname;
+    private List<String> volumes;
+    private List<String> ports;
+    private List<String> links;
+    private Integer wait;
 
     public void setName(String name) {
         this.name = name;
@@ -15,5 +25,37 @@ abstract class AbstractImage {
             throw new MojoExecutionException("image.name must not be null");
         }
         return name.trim().toLowerCase();
+    }
+
+    public void setRepository(String repository) {
+        this.repository = repository;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public void setVolumes(List<String> volumes) {
+        this.volumes = volumes;
+    }
+
+    public void setPorts(List<String> ports) {
+        this.ports = ports;
+    }
+
+    public void setLinks(List<String> links) {
+        this.links = links;
+    }
+
+    public void setWait(Integer wait) {
+        this.wait = wait;
     }
 }
